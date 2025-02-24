eb1fe1a22acce588a865c044466fd21b2ce6ebe7
mfatiralfarizi938 <mfatiralfarizi938@gmail.com>
2025-02-24 02:39:08 +0000

    dashboard

--- app/Http/Controllers/dashboardController.php
+++ app/Http/Controllers/dashboardController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\barangkeluar;
+use App\Models\pelanggan;
+use App\Models\stok;
+use App\Models\suplier;
 use Illuminate\Http\Request;
 
 class dashboardController extends Controller
 {
     public function index()
     {
-    return view('Dashboard.dashboard');
+        $getSupleir = suplier::count();
+        $getPelanggan= pelanggan::count();
+        $getStok = stok::count();
+        $getPendapatan = barangkeluar::sum('sub_total');
+
+    return view('Dashboard.dashboard', compact(
+        'getSupleir',
+        'getPelanggan',
+        'getStok',
+        'getPendapatan',
+    ));
     }
 }
